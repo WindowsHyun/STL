f8b5a341d0698d8ca1dffbdc7a1c8573bfdbaa00
WindowsHyun <kwon616@gmail.com>
2016-05-25 12:14:50 +0900

    20160525 STL 아이템박스, 컨테이너 등등 다양한 코딩을 배웠다.

--- 20160307STL/소스.cpp
+++ 20160307STL/소스.cpp
@@ -1,28 +1,73 @@
 //---------------------------------------------------------------------------
-// 2016년 05월 23일
+// 2016년 05월 25일
 //
 //---------------------------------------------------------------------------
 #include <iostream>
 #include <vector>
-#include <string>
-#include <set>
+#include <algorithm>
+#include <iterator>
 #include "save.h"
 
 using namespace std;
+/*
+439쪽 그 외 STL 컨테이너
+STL 알고리즘과 함께 어울려 작동하는 컨테이너를 만들 수 있는 방법은?
 
-class Game {
-	int n;
+1. STL 컨테이너가 요구하는 인터페이스를 제공한다.
+	- string을 stl 컨테이너로 이용하는 경우를 말한다.
+	- string은 begin(), end(), push_back()등을 제공한다.
+2. 컨테이너와 알고리즘간의 인터페이스를 담당하는 객체를 제공한다(반복자)
+	기존 배열
+	반복자는 포린터에 대하여 특수화(specializtion)되어 있으므로
+3. wrapper 클래스를 직접 만들어 사용한다.
+
+------------------------------------------------------------------------------------------
+표준을 준수하는 컨테이너를 만들어 보자.
+
+ItemBox 컨테이너 - 특별히 하는 일 없는 array<T, size>와 비슷한 컨테이너
+------------------------------------------------------------------------------------------
+*/
+template <class T, int n>
+class ItemBox {
+	T data[n];
+public:
+	T* begin() {
+		return data;
+		//return data;
+	};
+	T* end() {
+		return data + n;
+	}
+	size_t size() {
+		return n;
+	};
 };
 
-// set의 정렬기준은 두 가지로 설정할 수 있다.
-// 1. 탬플릿 파라미터
-// 2. 생성자 파라미터
-
 int main() {
-	//1.
-	set<Game, less<Game>> s;
-	//2.
-	//set <int> s( 기준 );
+	ItemBox<int, 10> a;
+	auto p = a.begin();
+	for ( int i = 0; i < a.size(); ++i )
+		*p++ = i + 1;
+
+	copy( a.begin(), a.end(), ostream_iterator<int>( cout, " " ) );
+	cout << endl;
+
+	//내림차순으로 정렬해보자
+
+	sort( a.begin(), a.end(), []( int a, int b ) {
+		return a > b;
+	} );
+
+	for ( int d : a )
+		cout << d << " ";
+	cout << endl;
+
+	// 원소의 순서를 순환, rotate
+	rotate( a.begin(), a.begin() + 2, a.end() );
+
+	for ( int d : a )
+		cout << d << " ";
+	cout << endl;
 
 	//save();
 }
